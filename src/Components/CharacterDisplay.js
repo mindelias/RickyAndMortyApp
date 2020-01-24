3f8a983b718f3b447c5941e2c434c54397311594
mindelias <mindelias@gmail.com>
2020-01-24 02:15:00 +0100

    get all data from /api/episodes

--- src/Components/CharacterDisplay.js
+++ src/Components/CharacterDisplay.js
@@ -6,27 +6,26 @@ import { Link } from "react-router-dom";
 //     <CardExampleCard/>
 // ))
 const CardExampleCard = ({ item }) => (
-    <Grid.Column>
-
-  <Card>
-    <Image src={item.image} wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>{item.name}</Card.Header>
-      <Card.Meta>
-        <span className="date">Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        {item.name} is {item.status}
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <Link>
-        <Icon name="user" />
-        Episode:{item.episode.length}
-      </Link>
-    </Card.Content>
-  </Card>
-    </Grid.Column>
+  <Grid.Column>
+    <Card>
+      <Image src={item.image} wrapped ui={false} />
+      <Card.Content>
+        <Card.Header>{item.name}</Card.Header>
+        <Card.Meta>
+          <span className="date">Joined in 2015</span>
+        </Card.Meta>
+        <Card.Description>
+          {item.name} is {item.status}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <Link>
+          <Icon name="user" />
+          Episode:{item.episode.length}
+        </Link>
+      </Card.Content>
+    </Card>
+  </Grid.Column>
 );
 
 export default CardExampleCard;
